c87d6f0d65ab8b3f294a9dfd9f8510f3aa2aec95
dayana-sog <dayanasog23@gmail.com>
2020-03-16 18:08:01 +0000

    Criação do Header Global

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
+import Header from './components/Header/index';
 import Routes from './routes';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes />
+      <GlobalStyle />
     </BrowserRouter>
   );
 }
